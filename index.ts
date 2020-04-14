4d472888247cc6549053dbf54533e06e3f7611fb
alex <batmandocode@gmail.com>
2020-04-14 21:27:45 +1000

    2-4 (finished)

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-const hello = (name: string) => {
-    console.log('testing')
-}
+const hello = (name: string, isMale: boolean, age: number) => {
+  console.log("testing");
+};
 
+let testingNumberA: number = undefined
+let testingNumberB: number = null
 
 // tsc index.ts
+// undefined and null : 所有类型的子类型
