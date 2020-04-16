87938aa6ef6a55605db82a07bd3735480da0f97d
alex <batmandocode@gmail.com>
2020-04-16 11:07:08 +1000

    2-12 (finished)

--- index.ts
+++ index.ts
@@ -1,126 +1,15 @@
-// class , Object, OOP
 
-// -> create an animal
-// * I just want add some new functions
-class Animal {
-  protected name: string; // * now the child class can access to the property
-  constructor(name) {
-    this.name = name;
-  }
-  run() {
-    return "the animal is running ";
-  }
+// -> enum 
+// in some case, we should replace all the export const with the enum
+enum Direction {
+    UP = 'UP',
+    DOWN = 'DOWN'
 }
 
-const cat = new Animal("hello catty");
-console.log(cat.name); // * name is protected, not access to it.
-console.log(cat.run());
+let x = Direction.DOWN;
 
-// -> extends the animal
-class Dog extends Animal {
-  // !! extends, there is no need to do another constructor
-  bark() {
-    console.log("dog is barking ...");
-  }
+// -> const enum: improve performence 
+// * only use const for const enum, rather than the computed enum 
+const enum Colors {
+    RED = 'RED'
 }
-
-const dog = new Dog("xiao bao");
-dog.bark();
-
-// -> restart and thing
-// * now we need another constructor
-class Human_A extends Animal {
-  humanGender: string;
-  constructor(humanGender, name) {
-    super(name);
-    this.name = name;
-    this.humanGender = humanGender;
-  }
-  giveGender() {
-    console.log(this.name, this.humanGender);
-  }
-}
-
-// -> now we want make the value private
-class Human_B extends Animal {
-  private humanGender: string; // * now you cannot access to the gender
-  constructor(humanGender, name) {
-    super(name);
-    this.name = name;
-    this.humanGender = humanGender;
-  }
-  private thisIsAnPrivateFunction() {
-    console.log("this is an private function");
-  }
-  giveGender() {
-    console.log(this.name, this.humanGender);
-  }
-}
-
-// -> read only: you can read it, but not change is
-class Human_C extends Animal {
-  readonly humanGender: string; // * now you cannot access to the gender
-  publicValue: string;
-  constructor(humanGender, name) {
-    super(name);
-    this.name = name;
-    this.humanGender = humanGender;
-  }
-  private thisIsAnPrivateFunction() {
-    console.log("this is an private function");
-  }
-  giveGender() {
-    console.log(this.name, this.humanGender);
-  }
-}
-
-const human_c = new Human_C("human c", "female");
-human_c.publicValue = "new value"; // * public
-human_c.humanGender = "new value"; // * read only
-
-// -> static for the class
-// * I think i am pretty good at it .
-// * static can be a value or a function
-
-// -> interface and inplements 
-// ! only the property is optional, but the function is not
-interface Radio {
-    switchVideo(): string;
-    test: () => string
-}
-
-interface Battery {
-  checkBattery(): string
-}
-
-// -> implement one interface 
-class Car implements Radio {
-  switchVideo(){
-    return 'hello'
-  }
-}
-
-// -> implement two interface 
-class CellPhone implements Radio, Battery {
-  switchVideo(){
-    return "hello" 
-  }
-  checkBattery(){
-    return 'hello'
-  }
-}
-
-// -> extands of the interface 
-interface RadioWithBattery extends Battery, Radio {
-
-}
-
-class AdvancedCard implements RadioWithBattery {
-  checkBattery(){
-    return '1'
-  }
-  switchVideo(){
-    return "2"
-  }
-}
-
