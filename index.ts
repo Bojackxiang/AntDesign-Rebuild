e708cbdd97ccc13307adc7e3ae50ab4b12235645
alex <batmandocode@gmail.com>
2020-04-16 12:21:51 +1000

    2-16 (finished)

--- index.ts
+++ index.ts
@@ -1,12 +1,35 @@
-// -> genertics 
-// * what problem it solves: I cannot make sure the input value type 
-function echo<T>(args: T): T {
-    return args
+type PlusType = (x: number, y: number) => number
+
+// -> type 
+function sum(x, y){
+    return x + y
 }
 
-const value = echo(10)
+// ❌ bad way to do it
+const sum_result_A: (x: number, y: number) => number = sum(1,2)
+// ✅ good way to do this
+const sum_result_B: PlusType = sum(1,2)
 
-function swap<T, U>(tuple: [T, U]): [U, T]{
-    return [tuple[1], tuple[0]]
+// ===========================================================================
+// -> 联合类型
+// * union: one part do the input, one part do the output
+type inputProps = number | string 
+type outputProps = number | string 
+
+const getName = (x:inputProps):outputProps => {
+    return 12
 }
+
+// ===========================================================================
+// * whenusing the union type, we can only access to the shared properties (只能访问共有属性)
+// !! us asseration to solve it
+const getName_A = (input: inputProps):outputProps => {
+    if((<string>input).length){
+        return (<string>input).length
+    }
+}
+
+
+
+
 
